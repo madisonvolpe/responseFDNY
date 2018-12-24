e7e7103defafd629145b1e1ed9f9b835dec2fb05
Madison Volpe <mv1617@nyu.edu>
2018-12-24 15:53:56 -0500

    some changes

--- Scripts/FDNY_Response.py
+++ Scripts/FDNY_Response.py
@@ -1,30 +1,31 @@
 
 # coding: utf-8
 
-# In[112]:
+# In[4]:
 
 
 #import pandas as pd 
 import pandas as pd 
-
+import datetime
+import time
 #read in data
-fdny_df = pd.read_csv('/Users/madisonvolpe/documents/Projects/FDNY_ResponseTimes/FDNY_Monthly_Response_Times.csv') 
+fdny_df = pd.read_csv('/Users/madisonvolpe/documents/Projects/FDNY_ResponseTimes/RawData/FDNY_Monthly_Response_Times.csv') 
 
 
-# In[113]:
+# In[17]:
 
 
 #inspect first five rows
 fdny_df.head()
 
 
-# In[114]:
+# In[18]:
 
 
 fdny_df.info() #inspecting data types of dataframe
 
 
-# In[115]:
+# In[19]:
 
 
 #Now we will filter for those in YEARMONTH with FY distinction
@@ -36,7 +37,7 @@ emer_inc = fdny_df.INCIDENTCLASSIFICATION.str.contains("All Fire/Emergency Incid
 fdny_df_fil = fdny_df_fil[emer_inc]
 
 
-# In[116]:
+# In[20]:
 
 
 #check the unique valaues of YEARMONTH and INCIDENTCLASSIFICATION columns 
@@ -44,16 +45,42 @@ print(fdny_df_fil.YEARMONTH.unique())
 print(fdny_df_fil.INCIDENTCLASSIFICATION.unique())
 
 
-# In[133]:
+# In[21]:
 
 
-#convert AVERAGERESPONSETIME to time 
-fdny_df_fil['AVERAGERESPONSETIME']=pd.to_datetime(fdny_df_fil['AVERAGERESPONSETIME'], format='%M:%S')
-fdny_df_fil
+#add hours via strftime 
+fdny_df_fil['AVERAGERESPONSETIME']=list(map(lambda x: datetime.datetime.strptime(x,'%M:%S').strftime('%H:%M:%S'),               fdny_df_fil['AVERAGERESPONSETIME']))
 
 
-# In[138]:
+# In[22]:
 
 
-fdny_df_fil.groupby('INCIDENTBOROUGH').agg({'INCIDENTCOUNT': 'mean'})
+fdny_df_fil.AVERAGERESPONSETIME.head()
+
+
+# In[23]:
+
+
+#convert to time 
+fdny_df_fil['AVERAGERESPONSETIME']=list(map(lambda x: time.strptime(x,'%H:%M:%S'),               fdny_df_fil['AVERAGERESPONSETIME']))
+
+
+# In[27]:
+
+
+#make seconds columns 
+fdny_df_fil['SECONDS'] = list(map(lambda x:datetime.timedelta                                  (hours=x.tm_hour,minutes=x.tm_min,seconds=x.tm_sec).total_seconds(),                                 fdny_df_fil['AVERAGERESPONSETIME']))
+
+
+# In[28]:
+
+
+fdny_df_fil.SECONDS.head()
+
+
+# In[29]:
+
+
+#this is the average incident count and average response time in seconds from FY 2010 to FY 2017 by borough! 
+fdny_df_fil.groupby('INCIDENTBOROUGH').agg({'INCIDENTCOUNT': 'mean', 'SECONDS': 'mean'})
 
